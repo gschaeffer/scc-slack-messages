97830bf90378ed43eca7dea0a89be529c8792154
Garrett Schaeffer <gschaeffer@gmail.com>
2021-04-15 14:20:54 -0500

    replace project id change with gcloud command from config environment

--- deploy_func.sh
+++ deploy_func.sh
@@ -5,7 +5,7 @@
 NAME=scc_slack_handler
 ENTRY_POINT=scc_slack_handler
 LABELS="app=scc_slack_messages"
-MEMORY=256MB
+MEMORY=128MB
 REGION=us-central1
 SOURCE_DIR="app/"
 TOPIC="scc-findings"
@@ -15,9 +15,8 @@ gcloud functions deploy $NAME \
     --memory $MEMORY \
     --region $REGION \
     --runtime python38 \
-    --set-env-vars "project_id=PROJECT_ID" \
+    --set-env-vars "project_id=$(gcloud config get-value project)" \
     --source $SOURCE_DIR \
     --trigger-topic $TOPIC \
     --update-labels $LABELS \
     --retry 
-    
